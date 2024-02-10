63a5947bf4fc417caa8a51866e7b7dd031bba15f
Brad Garropy <bradgarropy@gmail.com>
2024-02-09 22:04:58 -0600

    update title.

--- src/tests/_index.test.tsx
+++ src/tests/_index.test.tsx
@@ -10,5 +10,5 @@ test("renders", () => {
 
 test("meta", () => {
     const tags = meta(metaArgs)
-    expect(tags).toEqual([{title: "💿 remix starter | home"}])
+    expect(tags).toEqual([{title: "☁️ cloudinary motion demo"}])
 })
