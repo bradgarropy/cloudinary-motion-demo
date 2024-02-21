6a336f077385fd88ecccbcbed60ef7e15535daf7
Brad Garropy <bradgarropy@gmail.com>
2024-02-20 20:07:35 -0600

    address feedback.

--- src/routes/_index.tsx
+++ src/routes/_index.tsx
@@ -18,15 +18,18 @@ const cld = new Cloudinary({
 })
 
 const IndexRoute = () => {
-    const penguin = cld.image("cloudinary-motion-demo/penguin.gif").toURL()
-
+    // STEP 3: Use the Cloudinary SDK.
     const staticPenguin = cld
         .image("cloudinary-motion-demo/staticPenguin.jpg")
         .toURL()
 
-    // STEP 3: Extract a single frame of the gif.
+    const motionPenguin = cld
+        .image("cloudinary-motion-demo/motionPenguin.gif")
+        .toURL()
+
+    // STEP 4: Extract a single frame of the gif.
     const frozenPenguin = cld
-        .image("cloudinary-motion-demo/penguin.gif")
+        .image("cloudinary-motion-demo/motionPenguin.gif")
         .extract(getPage().byNumber(1))
         .toURL()
 
@@ -34,26 +37,42 @@ const IndexRoute = () => {
         <div className="mx-auto">
             <div className="grid grid-flow-col gap-x-16 place-content-center">
                 {/* STEP 1: Show a gif. */}
-                <img src={penguin} alt="penguin typing gif" />
+                <img
+                    src="https://res.cloudinary.com/bradgarropy/image/upload/cloudinary-motion-demo/motionPenguin.gif"
+                    alt="penguin typing gif"
+                />
 
                 {/* STEP 2: Respond to prefers-reduced-motion. */}
+                <picture>
+                    <source
+                        srcSet="https://res.cloudinary.com/bradgarropy/image/upload/cloudinary-motion-demo/staticPenguin.gif"
+                        media="(prefers-reduced-motion)"
+                    />
+
+                    <img
+                        src="https://res.cloudinary.com/bradgarropy/image/upload/cloudinary-motion-demo/motionPenguin.gif"
+                        alt="penguin typing gif"
+                    />
+                </picture>
+
+                {/* STEP 3: Use the Cloudinary SDK. */}
                 <picture>
                     <source
                         srcSet={staticPenguin}
                         media="(prefers-reduced-motion)"
                     />
 
-                    <img src={penguin} alt="penguin typing gif" />
+                    <img src={motionPenguin} alt="penguin typing gif" />
                 </picture>
 
-                {/* STEP 3: Display the frozen gif instead of a separate image. */}
+                {/* STEP 4: Display the frozen gif instead of a separate image. */}
                 <picture>
                     <source
                         srcSet={frozenPenguin}
                         media="(prefers-reduced-motion)"
                     />
 
-                    <img src={penguin} alt="penguin typing gif" />
+                    <img src={motionPenguin} alt="penguin typing gif" />
                 </picture>
             </div>
         </div>
